82b8395303ceeb468179c94f303b555c0ce8203f
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2021-12-09 16:45:51 +0100

    -

--- bonus/main.js
+++ bonus/main.js
@@ -1,18 +1,22 @@
 const hamburger = document.querySelector('.fa-bars');
-const showMore = document.getElementById('show_more');
 const hamburgerMenu = document.querySelector('.hamburger-menu');
-const showMoreFooter = document.querySelector('.show_more_footer');
 const close = document.querySelector('.close');
+const showMore = document.getElementById('show_more');
+const showMoreFooter = document.querySelector('.show_more_footer');
 const closeFooter = document.querySelector('.foot_close');
-hamburger.addEventListener('click', ()=>{
-    hamburgerMenu.classList.add('active');
-});
-close.addEventListener('click', ()=>{
-    hamburgerMenu.classList.remove('active');
-});
-showMore.addEventListener('click', ()=>{
-    showMoreFooter.classList.add('active');
-});
-closeFooter.addEventListener('click', ()=>{
-    showMoreFooter.classList.remove('active');
-});
+
+addClass(hamburger, 'click', hamburgerMenu, 'active')
+removeClass(close, 'click', hamburgerMenu, 'active')
+addClass(showMore, 'click', showMoreFooter, 'active')
+removeClass(closeFooter, 'click', showMoreFooter, 'active')
+
+function addClass(listener, listen,  x, myClass){
+    listener.addEventListener(listen, ()=>{
+        x.classList.add(myClass);
+    });
+}
+function removeClass(listener, listen,  x, myClass){
+    listener.addEventListener(listen, ()=>{
+        x.classList.removeClass(myClass);
+    });
+}
