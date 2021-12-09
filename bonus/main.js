dd40923256c152d7a29107767243774d135f8f7a
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2021-12-09 16:13:03 +0100

    -

--- bonus/main.js
+++ bonus/main.js
@@ -1,9 +1,18 @@
 const hamburger = document.querySelector('.fa-bars');
+const showMore = document.getElementById('show_more');
 const hamburgerMenu = document.querySelector('.hamburger-menu');
+const showMoreFooter = document.querySelector('.show_more_footer');
 const close = document.querySelector('.close');
+const closeFooter = document.querySelector('.foot_close');
 hamburger.addEventListener('click', ()=>{
     hamburgerMenu.classList.add('active');
 });
 close.addEventListener('click', ()=>{
     hamburgerMenu.classList.remove('active');
+});
+showMore.addEventListener('click', ()=>{
+    showMoreFooter.classList.add('active');
+});
+closeFooter.addEventListener('click', ()=>{
+    showMoreFooter.classList.remove('active');
 });
