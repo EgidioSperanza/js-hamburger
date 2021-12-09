c0ebccca72c587500fe7f41382c9f7d7e7f9b5d8
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2021-12-09 16:54:45 +0100

    i ; li salto spesso

--- bonus/main.js
+++ bonus/main.js
@@ -5,10 +5,10 @@ const showMore = document.getElementById('show_more');
 const showMoreFooter = document.querySelector('.show_more_footer');
 const closeFooter = document.querySelector('.foot_close');
 
-addClass(hamburger, 'click', hamburgerMenu, 'active')
-removeClass(close, 'click', hamburgerMenu, 'active')
-addClass(showMore, 'click', showMoreFooter, 'active')
-removeClass(closeFooter, 'click', showMoreFooter, 'active')
+addClass(hamburger, 'click', hamburgerMenu, 'active');
+removeClass(close, 'click', hamburgerMenu, 'active');
+addClass(showMore, 'click', showMoreFooter, 'active');
+removeClass(closeFooter, 'click', showMoreFooter, 'active');
 
 function addClass(listener, listen,  x, myClass){
     listener.addEventListener(listen, ()=>{
